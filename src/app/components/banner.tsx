cb1c3c012fd9d2ea76fd17dba64563dd200cbfba
aryelnascimento11 <aryeln897@gmail.com>
2023-10-09 15:53:46 -0300

    fazendo function de inserir parte 2

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -3,12 +3,14 @@ import Note from "./note"
 import { useState } from "react"
 
 export default function Banner() {
-    const [text, setText] = useState("oioi")
-    const item = [
+    const [newText, setNewText] = useState("")
+    const [items, setItems] = useState([
         "fake1",
         "fake2",
         "fake3",
     ]
+    )
+
 
     function café(ola: string) {
         return (
@@ -19,18 +21,23 @@ export default function Banner() {
     return (
         <>
             <section className="w-screen h-screen bg-black flex py-4 justify-center">
-                <div className="border border-white shadow-lg p-4  shadow-green-700 rounded-lg mt-10">
+                <div className="w-[30%]  min-w-[500px]  sm:min-w[100px] sm:h-[70vh] md:h-fit rounded-lg  border border-white shadow-lg  shadow-green-700mt-10 p-4">
                     <h1 className="text-3xl text-center py-4  text-white">Anotações</h1>
                     <div>
-                        <input className="rounded-full px-2 text-2xl p-1 " type="text" />
-                        <button className="text-white rounded   border hover:bg-[rgba(255,255,255,0.2)] hover:scale-110 cursor-pointer transition  ">adicionar</button>
+                        <input
+                            defaultValue={'Adicione seu texto'} onChange={(e) => { setNewText(e.target.value) }} className="rounded-full  px-2 text-2xl p-1 " type="text" />
+                        <button
+                            onClick={(o) => { setItems([...items, newText]) }}
+                            className="text-white rounded   border hover:bg-[rgba(255,255,255,0.2)] hover:scale-110 cursor-pointer transition  ">
+                            adicionar
+                        </button>
                     </div>
-                    <ul className="text-white divide-y border rounded-lg ">
-                        {item.map((ola) => (<Note key={ola} title={ola} />))}
+                    <ul className="text-white divide-y rounded-lg mt-2 border">
+                        {items.map((ola) => (<Note key={ola} title={ola} />))}
                     </ul>
                 </div>
             </section>
-
+            {/* border border-white shadow-lg p-4  shadow-green-700 rounded-lg mt-10 */}
         </>
     )
 }
--- src/app/components/note.tsx
+++ src/app/components/note.tsx
@@ -1,10 +1,13 @@
 "use client"
 
+import { useState } from "react"
 interface PropsNote {
     title: string,
 }
 
 export default function Note(props: PropsNote) {
+  
+    
     return (
         <ul className="text-white divide-y border rounded-lg "> 
            <li className="p-2 justify-between flex">
