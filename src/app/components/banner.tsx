7fcf0caa2c65643ea6406ca57c8855e069279e33
Aryel <126723967+aryelnascimento11@users.noreply.github.com>
2023-10-11 16:36:42 -0300

    fazendo function delete

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -3,39 +3,40 @@ import { list } from "postcss"
 import Note from "./note"
 import { useState } from "react"
 import { chown } from "fs"
+import { IItem } from "../types/item"
+
 
 export default function Banner() {
-    const [newText, setNewText] = useState("")
-    const [items, setItems] = useState([
-        "fake1",
-        "fake2",
-        "fake3",
+    const [newdelet, setNewdelet] = useState("")
+    const [newText, setNewText] = useState({
+        id:2,
+        title:"oi"
+    })
+    const [items, setItems] = useState<IItem[]>([
+       {id:1,title:"fake"}
     ]
     )
-    
+
     function delete_() {
-      items.splice(2)
-      console.log(items);
-        
+        items.splice(2)
+        console.log(items);
+
     }
-// - apagar o item
+    // - apagar o item
 
     return (
-        <>  
+        <>
             <section className="w-screen h-screen bg-black flex py-4 justify-center">
                 <div className="w-[30%]  min-w-[500px]  sm:min-w[100px] sm:h-[70vh] md:h-fit rounded-lg  border border-white shadow-lg  shadow-green-700mt-10 p-4">
-                    <button onClick={delete_}className="text-white rounded   border hover:bg-[rgba(255,255,255,0.2)] hover:scale-110 cursor-pointer transition  ">
-                        adicionar
-                    </button>
                     <h1 className="text-3xl text-center py-4  text-white">Anotações</h1>
                     <div className="justify-between flex">
-                        <input  onChange={(e) => { setNewText(e.target.value) }} className="rounded-full  px-2 text-2xl p-1 " type="text" />
-                        <button onClick={(o) => { setItems([...items, newText]) }}className="text-white rounded   border hover:bg-[rgba(255,255,255,0.2)] hover:scale-110 cursor-pointer transition  ">
+                        <input onChange={(e) => { setNewText({...newText, title:e.target.value}) }} className="rounded-full  px-2 text-2xl p-1 " type="text" />
+                        <button onClick={(o) => { setItems([...items, newText]) }} className="text-white rounded   border hover:bg-[rgba(255,255,255,0.2)] hover:scale-110 cursor-pointer transition  ">
                             adicionar
                         </button>
                     </div>
                     <ul className="text-white divide-y rounded-lg mt-2 border">
-                        {items.map((ola) => (<Note key={ola} title={ola} />))}
+                        {items.map((item) => (<Note key={item.id} item={item} />))}
                     </ul>
                 </div>
             </section>
--- src/app/components/note.tsx
+++ src/app/components/note.tsx
@@ -2,18 +2,22 @@
 
 import { list } from "postcss"
 import { useState } from "react"
+import { IItem } from "../types/item"
 interface PropsNote {
-    title: string,
+    item:IItem
 }
 
 
 export default function Note(props: PropsNote) {
+
     return (
-        <ul className="text-white divide-y border rounded-lg "> 
-           <li className="p-2 justify-between flex">
-          {props.title}
-            <button className="text-white p-1">X</button> 
-            </li>   
+        <ul className="text-white divide-y border rounded-lg ">
+            <li className="p-2 justify-between flex">
+                {props.item.title}
+                <button className="text-white rounded">
+                    X
+                </button>
+            </li>
         </ul>
     )
 }
